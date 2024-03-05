7f77d414f194165bd96e14ada5d859aac7b85dc9
Serhii Snitsaruk <limbonaut@pm.me>
2024-03-05 17:39:54 +0100

    Add SaveNodePos task

---
+++ ai/tasks/save_node_pos.gd
@@ -0,0 +1,37 @@
+#*
+#* save_node_pos.gd
+#* =============================================================================
+#* Copyright 2024 Serhii Snitsaruk
+#*
+#* Use of this source code is governed by an MIT-style
+#* license that can be found in the LICENSE file or at
+#* https://opensource.org/licenses/MIT.
+#* =============================================================================
+#*
+@tool
+extends BTAction
+## SaveNodePos: Save node position to blackboard and return SUCCESS.
+
+
+## Specify node.
+@export var node: BBNode = BBNode.new()
+
+## Specify blackboard variable to store result.
+@export var position_var: StringName = &"target_pos"
+
+
+func _generate_name() -> String:
+	return "SaveNodePos %s  %s" % [
+			node,
+			LimboUtility.decorate_output_var(position_var),
+		]
+
+
+func _tick(_delta: float) -> Status:
+	var node_inst = node.get_value(agent, blackboard)  # Note: Untyped to support both Node2D and Node3D
+	if is_instance_valid(node_inst):
+		blackboard.set_var(position_var, node_inst.global_position)
+		return SUCCESS
+	else:
+		push_warning("SaveNodePos: Failed to resolve node parameter: " + str(node))
+		return FAILURE
